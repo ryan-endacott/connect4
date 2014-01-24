cd9c03c404a2e84f9906f28837ac5dcabd586992
Ryan Endacott <rzeg24@gmail.com>
2014-01-23 22:31:11 -0600

    Player setup complete.

--- game.js
+++ game.js
@@ -1,31 +1,64 @@
+
+// Global variables
+var players = {};
+var unmatchedPlayers = [];
+
+// Constants
+var BOARDWIDTH = 8;
+var BOARDHEIGHT = 6;
+
 // Export entire game function
 module.exports = function(io) {
-
-  var players = {};
-  var unmatchedPlayers = [];
-
   io.sockets.on('connection', function(socket) {
-
-    socket.on('newPlayer', function(playerName) {
-
-      // if player with that name already exists
-      if (players[playerName] || unmatchedPlayers.indexOf(playerName) != -1) {
-        socket.emit('error', 'Player with that name already exists.');
-        return;
-      }
-
-      console.log("New player joining: " + playerName);
-
-      // If there is another unmatched player, start the game
-      if (unmatchedPlayers.length > 0) {
-        var opponent = unmatchedPlayers.pop();
-      }
-      else { // otherwise, add to unmatchedPlayers
-        unmatchedPlayers.push(playerName);
-      }
-
-    })
+    socket.on('newPlayerName', function(newPlayerName) {
+      handleNewPlayer(newPlayerName, socket);
+    });
   });
 
 };
 
+
+function handleNewPlayer(newPlayerName, socket) {
+  // if player with that name already exists
+  if (players[newPlayerName] || unmatchedPlayers.indexOf(newPlayerName) != -1) {
+    socket.emit('error', 'Player with that name already exists.');
+    return;
+  }
+
+  console.log("New player joining: " + newPlayerName);
+  var newPlayer = new Player(socket, null);
+  players[newPlayerName] = newPlayer;
+
+  // If there is another unmatched player, start the game
+  if (unmatchedPlayers.length > 0) {
+    var p2Name = unmatchedPlayers.pop();
+    var p2 = players[p2Name]
+    newPlayer.opponent = p2;
+    newPlayer.gameboard = p2.gameboard;
+    p2.opponent = newPlayer;
+    console.log("Matched " + opponent + " with " + newPlayerName);
+    playGame(newPlayer, p2);
+  }
+  else { // otherwise, add to unmatchedPlayers and create gameboard
+    newPlayerName.gameboard = createNewGameboard();
+    unmatchedPlayers.push(newPlayerName);
+  }
+}
+
+function playGame(player1, player2) {
+}
+
+function createNewGameboard(width, height) {
+  var gameboard = new Array(width);
+  for (var i = 0; i < width; i++) {
+    gameboard[i] = new Array(height);
+  }
+  return gameboard;
+}
+
+
+function Player(socket, opponent) {
+  this.socket = socket;
+  this.opponent = null;
+  this.gameboard = null;
+}
