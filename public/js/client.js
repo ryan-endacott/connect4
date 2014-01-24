b66bc25044579304e23ac99ae7595c1f31020a14
Ryan Endacott <rzeg24@gmail.com>
2014-01-23 18:15:22 -0600

    added canvas, drawing black box

--- public/js/client.js
+++ public/js/client.js
@@ -1,14 +1,29 @@
 
+$(document).ready(initialize);
 
+
+// Global variables
+var canvas;
+var ctx;
+var width;
+var height;
+var socket;
 
 // initialize game
 function initialize() {
 
-  var socket = io.connect('http://localhost');
+  socket = io.connect('http://localhost');
   socket.on('hi', function(data) {
     console.log(data);
     socket.emit('hi', 'whatup');
   })
+
+  canvas = document.getElementById('connect4');
+  ctx = canvas.getContext('2d');
+  width = canvas.width;
+  height = canvas.height;
+
+  ctx.fillRect(0, 0, width, height);
 
 }
 
